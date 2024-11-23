25b9fc636565772ed1cd02ce526f482e1ff18dff
GaryGaoyx <garygaoyx1@gmail.com>
2024-11-22 21:30:10 -0500

    Creating a Driver 7-Segment Display

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -1,178 +1,108 @@
-/* USER CODE BEGIN Header */
-/**
- ******************************************************************************
- * @file           : main.c
- * @brief          : Main program body
- ******************************************************************************
- * @attention
- *
- * Copyright (c) 2024 STMicroelectronics.
- * All rights reserved.
- *
- * This software is licensed under terms that can be found in the LICENSE file
- * in the root directory of this software component.
- * If no LICENSE file comes with this software, it is provided AS-IS.
- *
- ******************************************************************************
- */
-/* USER CODE END Header */
-/* Includes ------------------------------------------------------------------*/
 #include "main.h"
+#include "ds1302.h"
+#include <stdio.h>
 
-/* Private includes ----------------------------------------------------------*/
-/* USER CODE BEGIN Includes */
-
-/* USER CODE END Includes */
-
-/* Private typedef -----------------------------------------------------------*/
-/* USER CODE BEGIN PTD */
-
-/* USER CODE END PTD */
-
-/* Private define ------------------------------------------------------------*/
-/* USER CODE BEGIN PD */
-#define NUM_74HC595_CHIPS 4
-/* USER CODE END PD */
-
-/* Private macro -------------------------------------------------------------*/
-/* USER CODE BEGIN PM */
-
-/* USER CODE END PM */
-
-/* Private variables ---------------------------------------------------------*/
-
-/* USER CODE BEGIN PV */
-const uint8_t data[NUM_74HC595_CHIPS] = { 0x00, 0x00, 0x00, 0x00 };
-
-//Segment Definition (Common Anode)
-const uint8_t segmentMap[10] = {
-    0xC0, // 0
-    0xF9, // 1
-    0xA4, // 2
-    0xB0, // 3
-    0x99, // 4
-    0x92, // 5
-    0x82, // 6
-    0xF8, // 7
-    0x80, // 8
-    0x90  // 9
-};
-
-//Inverted Segment Definition (Common Anode)
-const uint8_t segmentMapInverted[10] = {
-    0x3F, // 0
-    0x06, // 1
-    0x5B, // 2
-    0x4F, // 3
-    0x66, // 4
-    0x6D, // 5
-    0x7D, // 6
-    0x07, // 7
-    0x7F, // 8
-    0x6F  // 9
-};
-/* USER CODE END PV */
-
-/* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
-static void MX_GPIO_Init(void);
-/* USER CODE BEGIN PFP */
+void MX_GPIO_Init(void);
 
-/* USER CODE END PFP */
+GPIO_PORT = GPIOB;
+#define NUM_74HC595_CHIPS 4
+uint8_t data[NUM_74HC595_CHIPS] = { 0xFF, 0xFF, 0xFF, 0xFF };
 
-/* Private user code ---------------------------------------------------------*/
-/* USER CODE BEGIN 0 */
+const uint8_t segmentMap[10] = {
+		0x67, // 0
+		0x67, // 1
+		0xA4, // 2
+		0xB0, // 3
+		0x99, // 4
+		0x92, // 5
+		0x82, // 6
+		0xF8, // 7
+		0x80, // 8
+		0x90  // 9
+		};
 
-/* USER CODE END 0 */
+const uint8_t segmentMapInverted[10] = {
+		0x3F, // 0
+		0x06, // 1
+		0x5B, // 2
+		0x4F, // 3
+		0x66, // 4
+		0x6D, // 5
+		0x7D, // 6
+		0x07, // 7
+		0x7F, // 8
+		0x6F  // 9
+		};
 
-/**
- * @brief  The application entry point.
- * @retval int
- */
 int main(void) {
-
-	/* USER CODE BEGIN 1 */
-
-	/* USER CODE END 1 */
-
-	/* MCU Configuration--------------------------------------------------------*/
-
-	/* Reset of all peripherals, Initializes the Flash interface and the Systick. */
 	HAL_Init();
-
-	/* USER CODE BEGIN Init */
-
-	/* USER CODE END Init */
-
-	/* Configure the system clock */
 	SystemClock_Config();
-
-	/* USER CODE BEGIN SysInit */
-
-	/* USER CODE END SysInit */
-
-	/* Initialize all configured peripherals */
 	MX_GPIO_Init();
-	/* USER CODE BEGIN 2 */
+	My_ds1302_init();
 
-	/* USER CODE END 2 */
+	Time_s user_seted_time = { 0 };
 
-	/* Infinite loop */
-	/* USER CODE BEGIN WHILE */
 	while (1) {
-
-
-		/* USER CODE END WHILE */
-
-		/* USER CODE BEGIN 3 */
+		//displayTime(0,0);
+		sendData();
+		HAL_Delay(1000);
 	}
-	/* USER CODE END 3 */
 }
-/* USER CODE BEGIN sendData */
+
 void sendData() {
-	HAL_GPIO_WritePin(GPIO_PORT, RCLK_Pin, GPIO_PIN_RESET);
-	HAL_GPIO_WritePin(GPIO_PORT, DATA_Pin, GPIO_PIN_RESET);
-	for (int i = 0; i < NUM_74HC595_CHIPS; i++) {
-		for (int j = 0; j < 8; j++) {
-			HAL_GPIO_WritePin(GPIO_PORT, SRCLK_Pin, GPIO_PIN_RESET);
+    HAL_GPIO_WritePin(GPIO_PORT, RCLK_Pin, GPIO_PIN_RESET);
+    for (int i = 0; i < NUM_74HC595_CHIPS; i++) {
+        for (int j = 0; j < 8; j++) {
+            uint8_t bitValue = (data[i] & (1 << (7 - j)));
 
-			uint8_t bitValue = (data[i] & (1 << (7 - j)));
+            if (bitValue) {
+                HAL_GPIO_WritePin(GPIO_PORT, DATA_Pin, GPIO_PIN_SET);
+            } else {
+                HAL_GPIO_WritePin(GPIO_PORT, DATA_Pin, GPIO_PIN_RESET);
+            }
 
-			if (bitValue) {
-			    HAL_GPIO_WritePin(GPIO_PORT, DATA_Pin, GPIO_PIN_SET);
-			} else {
-			    HAL_GPIO_WritePin(GPIO_PORT, DATA_Pin, GPIO_PIN_RESET);
-			}
-
-			HAL_GPIO_WritePin(GPIO_PORT, SRCLK_Pin, GPIO_PIN_SET);
-		}
-	}
-	HAL_GPIO_WritePin(GPIO_PORT, RCLK_Pin, GPIO_PIN_SET);
+            HAL_GPIO_WritePin(GPIO_PORT, SRCLK_Pin, GPIO_PIN_SET);
+            HAL_Delay(20);
+            HAL_GPIO_WritePin(GPIO_PORT, SRCLK_Pin, GPIO_PIN_RESET);
+        }
+    }
+    HAL_GPIO_WritePin(GPIO_PORT, RCLK_Pin, GPIO_PIN_SET);
 }
-/* USER CODE END sendData */
 
-/* USER CODE BEGIN displayTime */
 void displayTime(uint8_t hours, uint8_t minutes) {
 
-    data[0] = segmentMap[hours / 10]; //Tens of Hours
-    data[1] = segmentMap[hours % 10]; //Digits of Hours
+	data[0] = segmentMap[hours / 10]; //Tens of Hours
+	data[1] = segmentMap[hours % 10]; //Digits of Hours
 
-    //It may need to be inverted
-    //bc the third Segment Displays needs to be placed upside down
-    //to keep the dot up for simulating a colon HH:MM
-    //data[2] = segmentMapInverted[minutes / 10];
-    data[2] = segmentMap[minutes / 10]; //Tens of Minutes
+	//It may need to be inverted
+	//bc the third Segment Displays needs to be placed upside down
+	//to keep the dot up for simulating a colon HH:MM
+	//data[2] = segmentMapInverted[minutes / 10];
+	data[2] = segmentMap[minutes / 10]; //Tens of Minutes
 
-    data[3] = segmentMap[minutes % 10]; //Digits of Minutes
+	data[3] = segmentMap[minutes % 10]; //Digits of Minutes
 
-    sendData();
+	sendData();
 }
-/* USER CODE END displayTime */
 
-/**
- * @brief System Clock Configuration
- * @retval None
- */
+void My_ds1302_init(void) {
+	/* Initialize microcontroller configuration */
+	ds1302_init();
+	ds1302_clear_ram();
+	Time_s def_time = { 0 };
+	def_time.day = MONDAY;
+	def_time.year = 24;
+	def_time.month = 1;
+	def_time.date = 1;
+	def_time.clockSystem = DS1302_CLK_SYSTEM_12;
+	def_time.clockPeriod = DS1302_CLK_PM_PERIOD;
+	def_time.hour = 7;
+	def_time.min = 0;
+	def_time.sec = 0;
+	ds1302_set_time(&def_time);
+}
+
 void SystemClock_Config(void) {
 	RCC_OscInitTypeDef RCC_OscInitStruct = { 0 };
 	RCC_ClkInitTypeDef RCC_ClkInitStruct = { 0 };
@@ -207,58 +137,49 @@ void SystemClock_Config(void) {
 	}
 }
 
-/**
- * @brief GPIO Initialization Function
- * @param None
- * @retval None
- */
-static void MX_GPIO_Init(void) {
+void MX_GPIO_Init(void) {
 	GPIO_InitTypeDef GPIO_InitStruct = { 0 };
 	/* USER CODE BEGIN MX_GPIO_Init_1 */
 	/* USER CODE END MX_GPIO_Init_1 */
 
 	/* GPIO Ports Clock Enable */
-	__HAL_RCC_GPIOA_CLK_ENABLE();
+	__HAL_RCC_GPIOC_CLK_ENABLE();
 	__HAL_RCC_GPIOB_CLK_ENABLE();
+	__HAL_RCC_GPIOA_CLK_ENABLE();
 
 	/*Configure GPIO pin Output Level */
-	HAL_GPIO_WritePin(DATA_GPIO_Port, DATA_Pin, GPIO_PIN_RESET);
+	HAL_GPIO_WritePin(GPIOB,
+			RCLK_Pin | SRCLK_Pin | DATA_Pin | DS1302_PIN_SCLK_Pin
+					| DS1302_PIN_SDA_Pin | DS1302_PIN_RST_Pin, GPIO_PIN_RESET);
 
 	/*Configure GPIO pin Output Level */
-	HAL_GPIO_WritePin(GPIOB, RCLK_Pin | SRCLK_Pin, GPIO_PIN_RESET);
+	HAL_GPIO_WritePin(LED_Mode_GPIO_Port, LED_Mode_Pin, GPIO_PIN_RESET);
 
-	/*Configure GPIO pin : DATA_Pin */
-	GPIO_InitStruct.Pin = DATA_Pin;
-	GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+	/*Configure GPIO pins : Button_Mode_Pin Button_Minute_Pin Button_Hour_Pin */
+	GPIO_InitStruct.Pin = Button_Mode_Pin | Button_Minute_Pin | Button_Hour_Pin;
+	GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
 	GPIO_InitStruct.Pull = GPIO_PULLDOWN;
-	GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-	HAL_GPIO_Init(DATA_GPIO_Port, &GPIO_InitStruct);
+	HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
 
-	/*Configure GPIO pins : RCLK_Pin SRCLK_Pin */
-	GPIO_InitStruct.Pin = RCLK_Pin | SRCLK_Pin;
+	/*Configure GPIO pins : RCLK_Pin_Pin SRCLK_Pin_Pin DATA_Pin_Pin DS1302_PIN_SCLK_Pin
+	 DS1302_PIN_SDA_Pin DS1302_PIN_RST_Pin */
+	GPIO_InitStruct.Pin = RCLK_Pin | SRCLK_Pin | DATA_Pin
+			| DS1302_PIN_SCLK_Pin | DS1302_PIN_SDA_Pin | DS1302_PIN_RST_Pin;
 	GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
 	GPIO_InitStruct.Pull = GPIO_PULLDOWN;
 	GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
 	HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
-	/* USER CODE BEGIN MX_GPIO_Init_2 */
-	/* USER CODE END MX_GPIO_Init_2 */
+	/*Configure GPIO pin : LED_Mode_Pin */
+	GPIO_InitStruct.Pin = LED_Mode_Pin;
+	GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+	GPIO_InitStruct.Pull = GPIO_PULLDOWN;
+	GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+	HAL_GPIO_Init(LED_Mode_GPIO_Port, &GPIO_InitStruct);
 }
 
-/* USER CODE BEGIN 4 */
-
-/* USER CODE END 4 */
-
-/**
- * @brief  This function is executed in case of error occurrence.
- * @retval None
- */
 void Error_Handler(void) {
 	/* USER CODE BEGIN Error_Handler_Debug */
-	/* User can add his own implementation to report the HAL error return state */
-	__disable_irq();
-	while (1) {
-	}
 	/* USER CODE END Error_Handler_Debug */
 }
 
@@ -273,8 +194,6 @@ void Error_Handler(void) {
 void assert_failed(uint8_t *file, uint32_t line)
 {
   /* USER CODE BEGIN 6 */
-  /* User can add his own implementation to report the file name and line number,
-     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
   /* USER CODE END 6 */
 }
 #endif /* USE_FULL_ASSERT */
--- Debug/Core/Src/TM1637.d
+++ Debug/Core/Src/TM1637.d
@@ -1,4 +1,4 @@
-Core/Src/main.o: ../Core/Src/main.c ../Core/Inc/main.h \
+Core/Src/TM1637.o: ../Core/Src/TM1637.c ../Core/Inc/TM1637.h \
  ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h \
  ../Core/Inc/stm32f4xx_hal_conf.h \
  ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h \
@@ -23,9 +23,8 @@ Core/Src/main.o: ../Core/Src/main.c ../Core/Inc/main.h \
  ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h \
  ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h \
  ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h \
- ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h \
- ../Core/Inc/ds1302.h
-../Core/Inc/main.h:
+ ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h
+../Core/Inc/TM1637.h:
 ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h:
 ../Core/Inc/stm32f4xx_hal_conf.h:
 ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h:
@@ -51,4 +50,3 @@ Core/Src/main.o: ../Core/Src/main.c ../Core/Inc/main.h \
 ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h:
 ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h:
 ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h:
-../Core/Inc/ds1302.h:
